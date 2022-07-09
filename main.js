1123916a51c384172b466697dc05ac764b482d08
Yemil Godinez <yemilgr@gmail.com>
2022-07-09 17:52:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+import "./style.css";
+
+// setup typewriter effect in the terminal demo
+if (document.getElementsByClassName("terminal").length > 0) {
+	var i = 0;
+	var txt = `
+$ laravel new awseome-project
+Creating a "laravel/laravel" project
+- Downloading laravel/laravel
+- Installing laravel/laravel: Extracting archive
+
+Application ready! Build something amazing.
+
+$ cd awseome-project
+$ npm install && npm run dev 🚀
+`;
+	var speed = 60;
+
+	function typeItOut() {
+		if (i < txt.length) {
+			document.getElementsByClassName("terminal")[0].innerHTML += txt.charAt(i);
+			i++;
+			setTimeout(typeItOut, speed);
+		}
+	}
+
+	setTimeout(typeItOut, 1800);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    fontFamily: {
+      "pt-serif": ["PT Serif", "serif"],
+      "montserrat": ["Montserrat", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
