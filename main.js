a7a4762dfe152458313ddc6209e2634590d44a18
Yemil Arturo Godinez Rodríguez <yemilgr@gmail.com>
2022-12-21 16:57:14 +0000

    remove terminal

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 import "./style.css";
 
 // setup typewriter effect in the terminal demo
-if (document.getElementsByClassName("terminal").length > 0) {
-	var i = 0;
-	var txt = `
-$ laravel new awseome-project
-Creating a "laravel/laravel" project
-- Downloading laravel/laravel
-- Installing laravel/laravel: Extracting archive
+// if (document.getElementsByClassName("terminal").length > 0) {
+// 	var i = 0;
+// 	var txt = `
+// $ laravel new awseome-project
+// Creating a "laravel/laravel" project
+// - Downloading laravel/laravel
+// - Installing laravel/laravel: Extracting archive
 
-Application ready! Build something amazing.
+// Application ready! Build something amazing.
 
-$ cd awseome-project
-$ npm install && npm run dev 🚀
-`;
-	var speed = 60;
+// $ cd awseome-project
+// $ npm install && npm run dev 🚀
+// `;
+// 	var speed = 60;
 
-	function typeItOut() {
-		if (i < txt.length) {
-			document.getElementsByClassName("terminal")[0].innerHTML += txt.charAt(i);
-			i++;
-			setTimeout(typeItOut, speed);
-		}
-	}
+// 	function typeItOut() {
+// 		if (i < txt.length) {
+// 			document.getElementsByClassName("terminal")[0].innerHTML += txt.charAt(i);
+// 			i++;
+// 			setTimeout(typeItOut, speed);
+// 		}
+// 	}
 
-	setTimeout(typeItOut, 1800);
-}
+// 	setTimeout(typeItOut, 1800);
+// }
